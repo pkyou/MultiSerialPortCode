e9b3a8443e8f58fcfd6a73db93054594b1b72842
庞抗 <471162774@qq.com>
2016-12-15 15:54:24 +0800

    port manage test

--- MultiSerialPort/SerialPortManageTest/UnitTest1.cs
+++ MultiSerialPort/SerialPortManageTest/UnitTest1.cs
@@ -17,14 +17,26 @@ namespace SerialPortManageTest
                 BaudRate = 115200,
                 Parity = Parity.None,
                 DataBits = 8,
-                StopBits = StopBits.One,
+                StopBits = StopBits.One
             };
             PortManage portManage = new PortManage();
+            portManage.ReceiveCompleteEvent += Received;
             if (portManage.OpenPort(port))
             {
                 portManage.SendDataPacket(new byte[] {0x01, 0x02, 0x03, 0x04, 0x05});
             }
+            while (true)
+            {
+                ;
+            }
+        }
 
+        private void Received(object sender, byte[] data)
+        {
+            foreach (byte b in data)
+            {
+                Console.WriteLine(b);
+            }
         }
     }
 }
